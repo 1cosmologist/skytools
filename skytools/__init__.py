1effdc7b9d98dbf228f7fd22203ee9647754319d
Shamik Ghosh <thequarkexpress@gmail.com>
2024-04-26 14:09:28 -0700

    Add meta.yaml and licence boilerplates for conda

--- skytools/__init__.py
+++ skytools/__init__.py
@@ -1,3 +1,25 @@
+#######################################################################
+# This file is a part of SkyTools
+#
+# Sky Tools
+# Copyright (C) 2023  Shamik Ghosh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# For more information about SkyTools please visit 
+# <https://github.com/1cosmologist/skytools> or contact Shamik Ghosh 
+# at shamik@lbl.gov
+#
+#########################################################################
 """
 .. include:: ../README.md
 """
